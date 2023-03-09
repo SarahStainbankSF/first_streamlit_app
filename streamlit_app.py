e7f28949767b12d9083dba3d73395f09a0376a6b
SarahStainbankSF <127286657+SarahStainbankSF@users.noreply.github.com>
2023-03-09 23:28:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show)
 # New section to display advice
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 
 
 # Normalizing JSON format into human readable
